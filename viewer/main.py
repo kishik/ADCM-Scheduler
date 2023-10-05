766bbd7f763fcdf5fd712a8243905ada8b4843f5
Maxim Kishik <kishik.maxim@gmail.com>
2023-10-05 17:04:41 +0300

    fastapi error fix

--- viewer/main.py
+++ viewer/main.py
@@ -16,13 +16,14 @@ app = FastAPI()
 
 @app.post("/")
 async def deploy_project(project: Project):
-    k = 'https://drive.google.com/drive/folders/1BMz4mDAzFrxV3SkztJVD16clfWRKZ7Uc?usp=sharing'
+    k = project.link
     # match = re.search(r'd/.*/view', str(k)) 
     # print(match[0][2:-5] if match else 'Not found')
-    url = f"{k[:-12]}"
-    print(url)
-    gdown.download_folder(url, quiet=True, use_cookies=False, output=f'{project.name}')
-
+    url = f"{k[39:-12]}"
+    print(f'https://drive.google.com/drive/folders/{url}')
+    gdown.download_folder(f'https://drive.google.com/drive/folders/{url}', quiet=True, use_cookies=False, output=f'{project.name}')
+    os.system('ls')
     # os.system('wget --no-check-certificate \'https://docs.google.com/uc?export=download&id=FILEID\' -O FILENAME')
-    os.system(f'node ./xeokit-bim-viewer-app/createProject.js -p {project.name} -s ../{project.name}/**/*.ifc')
+    os.chdir('./xeokit-bim-viewer-app/')
+    os.system(f'node ./createProject.js -p {project.name} -s ../{project.name}/**/*.ifc')
     return 200
